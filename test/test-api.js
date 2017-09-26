06ce21319a8471829208f6b69efafbc5e324530c
Edward Jones <throne3d@gmail.com>
2017-09-26 22:42:20 +0100

    Ensure Client.send works as expected

--- test/test-api.js
+++ test/test-api.js
@@ -1,4 +1,5 @@
 var testHelpers = require('./helpers');
+var itWithCustomMock = testHelpers.itWithCustomMock;
 var chai = require('chai');
 var expect = chai.expect;
 var sinon = require('sinon');
@@ -30,6 +31,77 @@ describe('Client', function() {
       self.client.on('error', errorSpy);
       expect(wrap).not.to.throw();
       expect(errorSpy.args).to.deep.equal([[error]]);
+    });
+  });
+
+  describe('#send', function() {
+    testHelpers.hookMockSetup(beforeEach, afterEach);
+
+    beforeEach(function() {
+      this.debugSpy = sinon.spy();
+      this.connSpy = sinon.spy();
+      this.client.out.debug = this.debugSpy;
+      this.client.conn.write = this.connSpy;
+    });
+
+    it('works with simple data', function() {
+      this.client.send('JOIN', '#channel');
+      expect(this.debugSpy.args).to.deep.equal([
+        ['SEND:', 'JOIN #channel']
+      ]);
+      expect(this.connSpy.args).to.deep.equal([
+        ['JOIN #channel\r\n']
+      ]);
+    });
+
+    it('works with multiple arguments', function() {
+      this.client.send('TEST', 'example', 'data');
+      expect(this.debugSpy.args).to.deep.equal([
+        ['SEND:', 'TEST example data']
+      ]);
+      expect(this.connSpy.args).to.deep.equal([
+        ['TEST example data\r\n']
+      ]);
+    });
+
+    it('works with multi-word last parameter', function() {
+      this.client.send('TEST', 'example data');
+      expect(this.debugSpy.args).to.deep.equal([
+        ['SEND:', 'TEST :example data']
+      ]);
+      expect(this.connSpy.args).to.deep.equal([
+        ['TEST :example data\r\n']
+      ]);
+    });
+
+    itWithCustomMock('does not throw when disconnected',
+    {meta: {withoutServer: true}},
+    function() {
+      var self = this;
+      self.debugSpy = sinon.spy();
+      self.client.out.debug = self.debugSpy;
+      function wrap() {
+        self.client.send('TEST', 'example data');
+      }
+      expect(wrap).not.to.throw();
+      expect(self.debugSpy.args).to.deep.equal([
+        ['(Disconnected) SEND:', 'TEST :example data']
+      ]);
+    });
+
+    it('does not throw when disconnecting', function() {
+      var self = this;
+      function wrap() {
+        self.client.disconnect();
+        self.client.send('TEST', 'example data');
+      }
+      expect(wrap).not.to.throw();
+      expect(self.debugSpy.args).to.deep.include([
+        '(Disconnected) SEND:', 'TEST :example data'
+      ]);
+      expect(self.connSpy.args).to.deep.equal([
+        ['QUIT :node-irc says goodbye\r\n']
+      ]);
     });
   });
 });
