536c9bd58a1566c6a741ce868e6d8ed2ebbccc45
Edward Jones <throne3d@gmail.com>
2017-09-26 23:28:12 +0100

    Test Client.join, fix on different-case server channel

--- test/test-api.js
+++ test/test-api.js
@@ -104,4 +104,85 @@ describe('Client', function() {
       ]);
     });
   });
+
+  describe('#join', function() {
+    testHelpers.hookMockSetup(beforeEach, afterEach);
+
+    beforeEach(function(done) {
+      var self = this;
+      setTimeout(function() {
+        self.debugSpy = sinon.spy();
+        self.sendSpy = sinon.spy();
+        self.client.out.debug = self.debugSpy;
+        self.client.send = self.sendSpy;
+        done();
+      }, 10);
+    });
+
+    function sharedExamplesFor(channel, remoteChannel) {
+      function downcaseChannels(channels) {
+        return channels.map(function(x) { return x.toLowerCase(); });
+      }
+
+      it('works with given channel', function() {
+        this.client.join(channel);
+        expect(this.sendSpy.args).to.deep.equal([
+          ['JOIN', channel]
+        ]);
+      });
+
+      it('adds to opt.channels on successful join', function(done) {
+        var self = this;
+        self.client.on('join', function() {
+          setTimeout(check, 10);
+        });
+        expect(self.client.opt.channels).to.be.empty;
+        self.client.join(channel);
+        this.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChannel + '\r\n');
+
+        function check() {
+          expect(downcaseChannels(self.client.opt.channels)).to.deep.equal([channel.toLowerCase()]);
+          done();
+        }
+      });
+
+      it('calls given callback', function(done) {
+        var self = this;
+        expect(self.client.opt.channels).to.be.empty;
+        self.client.join(channel, check);
+        this.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChannel + '\r\n');
+
+        function check(nick, message) {
+          expect(nick).to.equal('testbot');
+          expect(message).to.deep.equal({
+            prefix: 'testbot!~testbot@EXAMPLE.HOST',
+            nick: 'testbot',
+            user: '~testbot',
+            host: 'EXAMPLE.HOST',
+            commandType: 'normal',
+            command: 'JOIN',
+            rawCommand: 'JOIN',
+            args: [remoteChannel]
+          });
+          done();
+        }
+      });
+    }
+
+    context('with same lowercase local and remote channel', function() {
+      sharedExamplesFor('#channel', '#channel');
+    });
+
+    context('with same mixed-case local and remote channel', function() {
+      sharedExamplesFor('#Channel', '#Channel');
+    });
+
+    context('with mixed-case local channel differing from lowercase remote channel', function() {
+      sharedExamplesFor('#Channel', '#channel');
+    });
+
+    context('with lowercase local channel differing from mixed-case remote channel', function() {
+      sharedExamplesFor('#channel', '#Channel');
+    });
+  });
 });
