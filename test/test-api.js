tikatoo/irc.js

commit d15c7a13dc3f8827eb2caa8440f0d81d7b3580a4

Fix .join to use a comma-separated list per IRC spec
As per https://tools.ietf.org/html/rfc2812#page-16, the client is
permitted to use a comma-separated list to communicate the channels it
wishes to join to the server. This is as opposed to a space-separated
list, as stated in the docs, which fails to do anything but join the
first channel on the servers I tested it on.

The server is *not* permitted to do the same back to the client (though
it only "should not", as opposed to being explicitly forbidden from
doing so.).

This commit also adds tests for the `.join` method of the `irc.Client`
API.

lib/irc.js
@@ -671,11 +671,11 @@ function Client(server, clientNick, opt) {
 
     self.addListener('kick', function(channel, nick) {
         if (self.opt.autoRejoin && nick.toLowerCase() === self.nick.toLowerCase())
-            self.send.apply(self, ['JOIN'].concat(channel.split(' ')));
+            self.send('JOIN', channel);
     });
     self.addListener('motd', function() {
         self.opt.channels.forEach(function(channel) {
-            self.send.apply(self, ['JOIN'].concat(channel.split(' ')));
+            self.send('JOIN', channel);
         });
     });
 
@@ -1041,19 +1041,22 @@ Client.prototype.cancelAutoRenick = function() {
     return oldInterval;
 };
 
-Client.prototype.join = function(channel, callback) {
-    var channelName = channel.split(' ')[0];
-    this.once('join' + channelName.toLowerCase(), function() {
-        // Append to opts.channel on successful join, so it rejoins on reconnect.
-        if (this.opt.channels.indexOf(channel) === -1) {
-            this.opt.channels.push(channel);
-        }
+Client.prototype.join = function(channelList, callback) {
+    var self = this;
+    var channels = channelList.split(',');
+    channels.forEach(function(channelName) {
+        self.once('join' + channelName.toLowerCase(), function() {
+            // Append to opts.channel on successful join, so it rejoins on reconnect.
+            if (self.opt.channels.indexOf(channelName) === -1) {
+                self.opt.channels.push(channelName);
+            }
 
-        if (typeof callback === 'function') {
-            return callback.apply(this, arguments);
-        }
+            if (typeof callback === 'function') {
+                return callback.apply(this, arguments);
+            }
+        });
     });
-    this.send.apply(this, ['JOIN'].concat(channel.split(' ')));
+    self.send('JOIN', channelList);
 };
 
 Client.prototype.part = function(channel, message, callback) {

test/test-api.js
@@ -119,29 +119,33 @@ describe('Client', function() {
       }, 10);
     });
 
-    function sharedExamplesFor(channel, remoteChannel) {
-      function downcaseChannels(channels) {
-        return channels.map(function(x) { return x.toLowerCase(); });
+    function sharedExamplesFor(channels, remoteChannels) {
+      function downcaseChannels(chans) {
+        return chans.map(function(x) { return x.toLowerCase(); });
       }
 
-      it('works with given channel', function() {
-        this.client.join(channel);
+      it('sends correct command', function() {
+        this.client.join(channels.join(','));
         expect(this.sendSpy.args).to.deep.equal([
-          ['JOIN', channel]
+          ['JOIN', channels.join(',')]
         ]);
       });
 
       it('adds to opt.channels on successful join', function(done) {
         var self = this;
+        var i = 0;
         self.client.on('join', function() {
-          setTimeout(check, 10);
+          i++;
+          if (i === channels.length) setTimeout(check, 10);
         });
         expect(self.client.opt.channels).to.be.empty;
-        self.client.join(channel);
-        this.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChannel + '\r\n');
+        self.client.join(channels.join(','));
+        remoteChannels.forEach(function(remoteChan) {
+          self.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChan + '\r\n');
+        });
 
         function check() {
-          expect(downcaseChannels(self.client.opt.channels)).to.deep.equal([channel.toLowerCase()]);
+          expect(downcaseChannels(self.client.opt.channels)).to.deep.equal(downcaseChannels(channels));
           done();
         }
       });
@@ -149,9 +153,12 @@ describe('Client', function() {
       it('calls given callback', function(done) {
         var self = this;
         expect(self.client.opt.channels).to.be.empty;
-        self.client.join(channel, check);
-        this.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChannel + '\r\n');
+        self.client.join(channels.join(','), check);
+        remoteChannels.forEach(function(remoteChan) {
+          self.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChan + '\r\n');
+        });
 
+        var i = 0;
         function check(nick, message) {
           expect(nick).to.equal('testbot');
           expect(message).to.deep.equal({
@@ -162,27 +169,35 @@ describe('Client', function() {
             commandType: 'normal',
             command: 'JOIN',
             rawCommand: 'JOIN',
-            args: [remoteChannel]
+            args: [remoteChannels[i]]
           });
-          done();
+          i++;
+          if (i === channels.length) done();
         }
       });
     }
 
     context('with same lowercase local and remote channel', function() {
-      sharedExamplesFor('#channel', '#channel');
+      sharedExamplesFor(['#channel'], ['#channel']);
     });
 
     context('with same mixed-case local and remote channel', function() {
-      sharedExamplesFor('#Channel', '#Channel');
+      sharedExamplesFor(['#Channel'], ['#Channel']);
     });
 
     context('with mixed-case local channel differing from lowercase remote channel', function() {
-      sharedExamplesFor('#Channel', '#channel');
+      sharedExamplesFor(['#Channel'], ['#channel']);
     });
 
     context('with lowercase local channel differing from mixed-case remote channel', function() {
-      sharedExamplesFor('#channel', '#Channel');
+      sharedExamplesFor(['#channel'], ['#Channel']);
+    });
+
+    context('with multiple channels', function() {
+      var localChannels = ['#channel', '#channel2', '#Test', '#Test2'];
+      var remoteChannels = ['#channel', '#Channel2', '#test', '#Test2'];
+
+      sharedExamplesFor(localChannels, remoteChannels);
     });
   });
 });

commit 532bb5a1fd82fef99ce3e3d64af4aae6ec4f1480

Fix client.join with a list of channel keys
This was erroneously broken in d15c7a13. It should be possible to call
join('#foo bar') to join the channel #foo with the channel key bar,
which this re-enables.

lib/irc.js
@@ -1084,7 +1084,10 @@ Client.prototype.cancelAutoRenick = function() {
 
 Client.prototype.join = function(channelList, callback) {
     var self = this;
-    var channels = channelList.split(',');
+    var parts = channelList.split(' ');
+    var channels = parts[0];
+    // var keys = parts[1];
+    channels = channels.split(',');
     channels.forEach(function(channelName) {
         self.once('join' + channelName.toLowerCase(), function() {
             // Append to opts.channel on successful join, so it rejoins on reconnect.
@@ -1097,7 +1100,7 @@ Client.prototype.join = function(channelList, callback) {
             }
         });
     });
-    self.send('JOIN', channelList);
+    self.send.apply(this, ['JOIN'].concat(channelList.split(' ')));
 };
 
 Client.prototype.part = function(channelList, message, callback) {

test/test-api.js
@@ -255,6 +255,48 @@ describe('Client', function() {
           if (i === channels.length) done();
         }
       });
+
+      it('sends right command with key', function() {
+        var self = this;
+        function wrap() {
+          self.client.join(channels.join(',') + ' ' +  channels.map(function() { return 'key'; }).join(','));
+        }
+        expect(wrap).not.to.throw();
+        expect(this.sendSpy.args).to.deep.equal([
+          ['JOIN', channels.join(','), channels.map(function() { return 'key'; }).join(',')]
+        ]);
+      });
+
+      it('works with key', function(done) {
+        var self = this;
+        self.client.opt.channels = [channels[0]];
+        self.client.join(channels.join(',') + ' ' +  channels.map(function() { return 'key'; }).join(','), check);
+        remoteChannels.forEach(function(remoteChan) {
+          self.mock.send(':testbot!~testbot@EXAMPLE.HOST JOIN :' + remoteChan + '\r\n');
+        });
+
+        var i = 0;
+        function check(nick, message) {
+          expect(nick).to.equal('testbot');
+          expect(message).to.deep.equal({
+            prefix: 'testbot!~testbot@EXAMPLE.HOST',
+            nick: 'testbot',
+            user: '~testbot',
+            host: 'EXAMPLE.HOST',
+            commandType: 'normal',
+            command: 'JOIN',
+            rawCommand: 'JOIN',
+            args: [remoteChannels[i]]
+          });
+          i++;
+          if (i === channels.length) end();
+        }
+
+        function end() {
+          expect(downcaseChannels(self.client.opt.channels)).to.deep.equal(downcaseChannels(channels));
+          done();
+        }
+      });
     }
 
     context('with same lowercase local and remote channel', function() {